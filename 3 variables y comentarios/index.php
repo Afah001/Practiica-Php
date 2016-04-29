5b814b8a4c7fd3cb6e4ced81f7c1bb7caf276197
Anderson Alarcon <anderson.alarcon.afa@gmail.com>
2016-04-28 21:25:42 -0500

    1

---
+++ 3 variables y comentarios/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>comentarios_Variables Php</title>
+	</head>
+	<body>
+		
+		<?php
+					//comentario de una linea 
+					
+					/*comentario
+					de multiples lineas*/
+
+					#otro comentario
+
+					//VARIABLES
+
+				$nombreUsuario="Anderson";
+				$edad=19;
+				$peso=1.5;
+				$soltero=false;
+
+				echo "su nombre es $nombreUsuario</br>" , "la edad: $edad</br>";    #variables en php
+				print "$nombreUsuario","$edad";  // aqui hay un error , print no  permite mortrar texto de esa forma
+				
+		?>
+
+	</body>
+</html>
